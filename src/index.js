6cef2c78db7d46a1688b9c66c65054a2e3ce3864
khovricheva <kseniyap1102@gmail.com>
2020-10-20 17:23:32 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length === 0 || undefined) return true;
+    if (str.length % 2 !== 0) return false;
+
+    let openBrackets = "";
+    let closeBrackets = "";
+    let sequence = [str[0]];
+
+    for (let elem of bracketsConfig) {
+        openBrackets += elem[0];
+        closeBrackets += elem[1];
+    }
+
+    for (let i = 1; i < str.length; i++) {
+        let currentBracket = str[i];
+        let lastBracket = sequence[sequence.length - 1];
+        if (
+            closeBrackets.includes(currentBracket) &&
+            closeBrackets.indexOf(currentBracket) ===
+                openBrackets.indexOf(lastBracket)
+        ) {
+            sequence.pop();
+        } else {
+            sequence.push(currentBracket);
+        }
+    }
+    return sequence.length === 0;
+};
